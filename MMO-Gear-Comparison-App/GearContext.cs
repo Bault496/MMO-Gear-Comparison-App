ae2430d246a62fb6612675c4e715bb71cd51c4a5
Bart Ault <115667577+Bault496@users.noreply.github.com>
2023-08-03 15:41:44 -0700

    Added a bit of stuff with EF core to get a database creating class and then create a database

---
+++ MMO-Gear-Comparison-App/GearContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMO_Gear_Comparison_App
+{
+    public class GearContext : DbContext
+    {
+        public GearContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // Database = The name for our database
+            // Server = the local server we're using
+            // Trusted_Connection = indicates that our windows account should be used
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MMOGearComparsionApp;Trusted_Connection=True;");
+        }
+        // Tell EF Core ot track Gear in the database
+        public DbSet<Gear> Gears { get; set; } 
+    }
+}
