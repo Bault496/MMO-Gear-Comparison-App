515beeb1f8b45a464f7f8c03a69ca53fbb812a60
davischristianson <dav.christian10@gmail.com>
2023-08-03 23:53:41 -0700

    Created labels and textboxes for a user to input information in the correct spots to create the gear

--- MMO-Gear-Comparison-App/CreateGearForm.Designer.cs
+++ MMO-Gear-Comparison-App/CreateGearForm.Designer.cs
@@ -30,6 +30,28 @@
         {
             btnSubmitGear = new Button();
             btnCancel = new Button();
+            lblItemName = new Label();
+            txtGearName = new TextBox();
+            txtGearLevel = new TextBox();
+            txtDamageStat = new TextBox();
+            txtArmorStat = new TextBox();
+            txtPrimaryStatType = new TextBox();
+            txtPrimaryStat = new TextBox();
+            lblGearLevel = new Label();
+            lblDamageStat = new Label();
+            lblArmorStat = new Label();
+            lblPrimaryStatType = new Label();
+            lblPrimaryStat = new Label();
+            lblSecondaryStatType = new Label();
+            lblSecondaryStat = new Label();
+            lblDurability = new Label();
+            lblGearSlot = new Label();
+            lblGearType = new Label();
+            txtSecondaryStatType = new TextBox();
+            txtSecondaryStat = new TextBox();
+            txtDurability = new TextBox();
+            txtGearSlot = new TextBox();
+            txtGearType = new TextBox();
             SuspendLayout();
             // 
             // btnSubmitGear
@@ -52,21 +74,242 @@
             btnCancel.UseVisualStyleBackColor = true;
             btnCancel.Click += btnCancel_Click;
             // 
+            // lblItemName
+            // 
+            lblItemName.AutoSize = true;
+            lblItemName.Location = new Point(48, 30);
+            lblItemName.Name = "lblItemName";
+            lblItemName.Size = new Size(69, 15);
+            lblItemName.TabIndex = 2;
+            lblItemName.Text = "Gear Name:";
+            // 
+            // txtGearName
+            // 
+            txtGearName.Location = new Point(144, 27);
+            txtGearName.Name = "txtGearName";
+            txtGearName.Size = new Size(100, 23);
+            txtGearName.TabIndex = 3;
+            // 
+            // txtGearLevel
+            // 
+            txtGearLevel.Location = new Point(144, 66);
+            txtGearLevel.Name = "txtGearLevel";
+            txtGearLevel.Size = new Size(100, 23);
+            txtGearLevel.TabIndex = 4;
+            // 
+            // txtDamageStat
+            // 
+            txtDamageStat.Location = new Point(144, 109);
+            txtDamageStat.Name = "txtDamageStat";
+            txtDamageStat.Size = new Size(100, 23);
+            txtDamageStat.TabIndex = 5;
+            // 
+            // txtArmorStat
+            // 
+            txtArmorStat.Location = new Point(144, 155);
+            txtArmorStat.Name = "txtArmorStat";
+            txtArmorStat.Size = new Size(100, 23);
+            txtArmorStat.TabIndex = 6;
+            // 
+            // txtPrimaryStatType
+            // 
+            txtPrimaryStatType.Location = new Point(144, 203);
+            txtPrimaryStatType.Name = "txtPrimaryStatType";
+            txtPrimaryStatType.Size = new Size(100, 23);
+            txtPrimaryStatType.TabIndex = 7;
+            // 
+            // txtPrimaryStat
+            // 
+            txtPrimaryStat.Location = new Point(144, 246);
+            txtPrimaryStat.Name = "txtPrimaryStat";
+            txtPrimaryStat.Size = new Size(100, 23);
+            txtPrimaryStat.TabIndex = 8;
+            // 
+            // lblGearLevel
+            // 
+            lblGearLevel.AutoSize = true;
+            lblGearLevel.Location = new Point(53, 69);
+            lblGearLevel.Name = "lblGearLevel";
+            lblGearLevel.Size = new Size(64, 15);
+            lblGearLevel.TabIndex = 9;
+            lblGearLevel.Text = "Gear Level:";
+            // 
+            // lblDamageStat
+            // 
+            lblDamageStat.AutoSize = true;
+            lblDamageStat.Location = new Point(40, 112);
+            lblDamageStat.Name = "lblDamageStat";
+            lblDamageStat.Size = new Size(77, 15);
+            lblDamageStat.TabIndex = 10;
+            lblDamageStat.Text = "Damage Stat:";
+            // 
+            // lblArmorStat
+            // 
+            lblArmorStat.AutoSize = true;
+            lblArmorStat.Location = new Point(50, 158);
+            lblArmorStat.Name = "lblArmorStat";
+            lblArmorStat.Size = new Size(67, 15);
+            lblArmorStat.TabIndex = 11;
+            lblArmorStat.Text = "Armor Stat:";
+            // 
+            // lblPrimaryStatType
+            // 
+            lblPrimaryStatType.AutoSize = true;
+            lblPrimaryStatType.Location = new Point(16, 206);
+            lblPrimaryStatType.Name = "lblPrimaryStatType";
+            lblPrimaryStatType.Size = new Size(101, 15);
+            lblPrimaryStatType.TabIndex = 12;
+            lblPrimaryStatType.Text = "Primary Stat Type:";
+            // 
+            // lblPrimaryStat
+            // 
+            lblPrimaryStat.AutoSize = true;
+            lblPrimaryStat.Location = new Point(43, 249);
+            lblPrimaryStat.Name = "lblPrimaryStat";
+            lblPrimaryStat.Size = new Size(74, 15);
+            lblPrimaryStat.TabIndex = 13;
+            lblPrimaryStat.Text = "Primary Stat:";
+            // 
+            // lblSecondaryStatType
+            // 
+            lblSecondaryStatType.AutoSize = true;
+            lblSecondaryStatType.Location = new Point(271, 30);
+            lblSecondaryStatType.Name = "lblSecondaryStatType";
+            lblSecondaryStatType.Size = new Size(115, 15);
+            lblSecondaryStatType.TabIndex = 14;
+            lblSecondaryStatType.Text = "Secondary Stat Type:";
+            // 
+            // lblSecondaryStat
+            // 
+            lblSecondaryStat.AutoSize = true;
+            lblSecondaryStat.Location = new Point(298, 69);
+            lblSecondaryStat.Name = "lblSecondaryStat";
+            lblSecondaryStat.Size = new Size(88, 15);
+            lblSecondaryStat.TabIndex = 15;
+            lblSecondaryStat.Text = "Secondary Stat:";
+            // 
+            // lblDurability
+            // 
+            lblDurability.AutoSize = true;
+            lblDurability.Location = new Point(325, 112);
+            lblDurability.Name = "lblDurability";
+            lblDurability.Size = new Size(61, 15);
+            lblDurability.TabIndex = 16;
+            lblDurability.Text = "Durability:";
+            // 
+            // lblGearSlot
+            // 
+            lblGearSlot.AutoSize = true;
+            lblGearSlot.Location = new Point(329, 158);
+            lblGearSlot.Name = "lblGearSlot";
+            lblGearSlot.Size = new Size(57, 15);
+            lblGearSlot.TabIndex = 17;
+            lblGearSlot.Text = "Gear Slot:";
+            // 
+            // lblGearType
+            // 
+            lblGearType.AutoSize = true;
+            lblGearType.Location = new Point(325, 206);
+            lblGearType.Name = "lblGearType";
+            lblGearType.Size = new Size(61, 15);
+            lblGearType.TabIndex = 18;
+            lblGearType.Text = "Gear Type:";
+            // 
+            // txtSecondaryStatType
+            // 
+            txtSecondaryStatType.Location = new Point(401, 27);
+            txtSecondaryStatType.Name = "txtSecondaryStatType";
+            txtSecondaryStatType.Size = new Size(100, 23);
+            txtSecondaryStatType.TabIndex = 20;
+            // 
+            // txtSecondaryStat
+            // 
+            txtSecondaryStat.Location = new Point(401, 66);
+            txtSecondaryStat.Name = "txtSecondaryStat";
+            txtSecondaryStat.Size = new Size(100, 23);
+            txtSecondaryStat.TabIndex = 21;
+            // 
+            // txtDurability
+            // 
+            txtDurability.Location = new Point(401, 109);
+            txtDurability.Name = "txtDurability";
+            txtDurability.Size = new Size(100, 23);
+            txtDurability.TabIndex = 22;
+            // 
+            // txtGearSlot
+            // 
+            txtGearSlot.Location = new Point(401, 155);
+            txtGearSlot.Name = "txtGearSlot";
+            txtGearSlot.Size = new Size(100, 23);
+            txtGearSlot.TabIndex = 23;
+            // 
+            // txtGearType
+            // 
+            txtGearType.Location = new Point(401, 203);
+            txtGearType.Name = "txtGearType";
+            txtGearType.Size = new Size(100, 23);
+            txtGearType.TabIndex = 24;
+            // 
             // CreateGearForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(txtGearType);
+            Controls.Add(txtGearSlot);
+            Controls.Add(txtDurability);
+            Controls.Add(txtSecondaryStat);
+            Controls.Add(txtSecondaryStatType);
+            Controls.Add(lblGearType);
+            Controls.Add(lblGearSlot);
+            Controls.Add(lblDurability);
+            Controls.Add(lblSecondaryStat);
+            Controls.Add(lblSecondaryStatType);
+            Controls.Add(lblPrimaryStat);
+            Controls.Add(lblPrimaryStatType);
+            Controls.Add(lblArmorStat);
+            Controls.Add(lblDamageStat);
+            Controls.Add(lblGearLevel);
+            Controls.Add(txtPrimaryStat);
+            Controls.Add(txtPrimaryStatType);
+            Controls.Add(txtArmorStat);
+            Controls.Add(txtDamageStat);
+            Controls.Add(txtGearLevel);
+            Controls.Add(txtGearName);
+            Controls.Add(lblItemName);
             Controls.Add(btnCancel);
             Controls.Add(btnSubmitGear);
             Name = "CreateGearForm";
             Text = "Create Gear";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button btnSubmitGear;
         private Button btnCancel;
+        private Label lblItemName;
+        private TextBox txtGearName;
+        private TextBox txtGearLevel;
+        private TextBox txtDamageStat;
+        private TextBox txtArmorStat;
+        private TextBox txtPrimaryStatType;
+        private TextBox txtPrimaryStat;
+        private Label lblGearLevel;
+        private Label lblDamageStat;
+        private Label lblArmorStat;
+        private Label lblPrimaryStatType;
+        private Label lblPrimaryStat;
+        private Label lblSecondaryStatType;
+        private Label lblSecondaryStat;
+        private Label lblDurability;
+        private Label lblGearSlot;
+        private Label lblGearType;
+        private TextBox txtSecondaryStatType;
+        private TextBox txtSecondaryStat;
+        private TextBox txtDurability;
+        private TextBox txtGearSlot;
+        private TextBox txtGearType;
     }
 }
