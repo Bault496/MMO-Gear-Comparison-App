648d576c34b1fa6d1627c836d7aa1a78ddd5e7b4
davischristianson <dav.christian10@gmail.com>
2023-08-12 22:43:13 -0700

    Added functionality to add gear to the database

--- MMO-Gear-Comparison-App/CreateGearForm.cs
+++ MMO-Gear-Comparison-App/CreateGearForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +20,39 @@ namespace MMO_Gear_Comparison_App
 
         private void btnSubmitGear_Click(object sender, EventArgs e)
         {
+            AddGear();
+        }
 
+        private void AddGear()
+        {
+            Gear gear = new Gear()
+            {
+                ItemName = txtGearName.Text,
+                ItemLevel = Convert.ToInt32(txtGearLevel.Text),
+                DamageScaling = Convert.ToInt32(txtDamageStat.Text),
+                ArmorScaling = Convert.ToInt32(txtArmorStat.Text),
+                primaryStat = txtPrimaryStatType.Text,
+                primaryStatScaling = Convert.ToInt32(txtPrimaryStat),
+                secondaryStat = txtSecondaryStatType.Text,
+                secondaryStatScaling = Convert.ToInt32(txtSecondaryStat.Text),
+                Durability = Convert.ToInt32(txtDurability.Text),
+                GearSlot = txtGearSlot.Text,
+                GearType = txtGearType.Text
+            };
+
+            try
+            {
+                using GearContext dbContext = new GearContext();
+
+                dbContext.Gears.Add(gear);
+                dbContext.SaveChanges();
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Database is not available right now. Please try again later.", "Database Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
