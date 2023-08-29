6bf8e77094f011b210be1b1b081fe9951b8f2fb6
davischristianson <dav.christian10@gmail.com>
2023-08-28 22:54:42 -0700

    Fixed database so now the create gear functions successfully creates the object and puts it into the database

---
+++ MMO-Gear-Comparison-App/Migrations/20230829055054_Intial.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MMO_Gear_Comparison_App.Migrations
+{
+    /// <inheritdoc />
+    public partial class Intial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Gears",
+                columns: table => new
+                {
+                    ItemID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ItemLevel = table.Column<int>(type: "int", nullable: false),
+                    DamageScaling = table.Column<int>(type: "int", nullable: true),
+                    ArmorScaling = table.Column<int>(type: "int", nullable: true),
+                    PrimaryStat = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PrimaryStatScaling = table.Column<int>(type: "int", nullable: true),
+                    SecondaryStat = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecondaryStatScaling = table.Column<int>(type: "int", nullable: true),
+                    Durability = table.Column<int>(type: "int", nullable: true),
+                    GearSlot = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GearType = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gears", x => x.ItemID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Gears");
+        }
+    }
+}
