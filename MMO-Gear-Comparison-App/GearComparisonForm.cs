20467ae61a9ab7bcc4973978ec017f6e022d44f5
davischristianson <dav.christian10@gmail.com>
2023-08-31 03:52:13 -0700

    Implemented cboSecondGear_SelectedIndexChanged, PopulateFirstGearStats and PopulateSecondGearStats

--- MMO-Gear-Comparison-App/GearComparisonForm.cs
+++ MMO-Gear-Comparison-App/GearComparisonForm.cs
@@ -37,10 +37,9 @@ namespace MMO_Gear_Comparison_App
 
                 foreach (Gear g in gearList)
                 {
-                    cboFirstGear.Items.Add(g.ItemName);
-                    cboSecondGear.Items.Add(g.ItemName);
+                    cboFirstGear.Items.Add(g);
+                    cboSecondGear.Items.Add(g);
                 }
-
             }
             catch (SqlException)
             {
@@ -67,16 +66,90 @@ namespace MMO_Gear_Comparison_App
         // When the user changes their gear selection - first gear
         private void cboFirstGear_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // Checks to see if the combo boxes are unselected
+            if(cboFirstGear.SelectedIndex != -1)
+            {
+                // Create a gear object to hold selected gear
+                Gear selectedGear = new Gear();
 
+                // Grabs the selected gear 
+                selectedGear = (Gear)cboFirstGear.Items[cboFirstGear.SelectedIndex];
+
+                // Send the gear to method PopulateFirstGearStats(Gear gear) to populate the stats in their
+                // respective text boxes
+                PopulateFirstGearStats(selectedGear);
+            }
         }
 
         // When the user changes their gear selection - second gear
         private void cboSecondGear_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if(cboSecondGear.SelectedIndex != -1)
+            {
+                // Create a gear object to hold selected gear
+                Gear selectedGear = new Gear();
+
+                // Grabs the selected gear
+                selectedGear = (Gear)cboFirstGear.Items[cboSecondGear.SelectedIndex];
+
+                // Send the gear to method PopulateFirstGearStats(Gear gear) to populate the stats in their
+                // respective text boxes
+                PopulateSecondGearStats(selectedGear);
+            }
+        }
+
+        /// <summary>
+        /// This will take an object gear that will have it stats 
+        /// distributed to their respective text boxes
+        /// </summary>
+        /// <param name="gear"></param>
+        private void PopulateFirstGearStats(Gear gear)
+        {
+            txtGearNameFirstGear.Text = gear.ItemName;
+            txtGearLevelFirstGear.Text = gear.ItemLevel.ToString();
+            txtDamageStatFirstGear.Text = gear.DamageScaling.ToString();
+            txtArmorStatFirstGear.Text = gear.ArmorScaling.ToString();
+            txtPrimaryStatTypeFirstGear.Text = gear.PrimaryStat;
+            txtPrimaryStatFirstGear.Text = gear.PrimaryStatScaling.ToString();
+            txtSecondaryStatTypeFirstGear.Text = gear.SecondaryStat;
+            txtSecondaryStatFirstGear.Text = gear.SecondaryStatScaling.ToString();
+            txtDurabilityFirstGear.Text = gear.Durability.ToString();
+            txtGearSlotFirstGear.Text = gear.GearSlot;
+            txtGearTypeFirstGear.Text = gear.GearType;
+        }
+
+        /// <summary>
+        /// This will take an object gear that will have it stats 
+        /// distributed to their respective text boxes
+        /// </summary>
+        /// <param name="gear"></param>
+        private void PopulateSecondGearStats(Gear gear)
+        {
+            txtGearNameSecondGear.Text = gear.ItemName;
+            txtGearLevelSecondGear.Text = gear.ItemLevel.ToString();
+            txtDamageStatSecondGear.Text = gear.DamageScaling.ToString();
+            txtArmorStatSecondGear.Text = gear.ArmorScaling.ToString();
+            txtPrimaryStatTypeSecondGear.Text = gear.PrimaryStat;
+            txtPrimaryStatSecondGear.Text = gear.PrimaryStatScaling.ToString();
+            txtSecondaryStatTypeSecondGear.Text = gear.SecondaryStat;
+            txtSecondaryStatSecondGear.Text = gear.SecondaryStatScaling.ToString();
+            txtDurabilitySecondGear.Text = gear.Durability.ToString();
+            txtGearSlotSecondGear.Text = gear.GearSlot;
+            txtGearTypeSecondGear.Text = gear.GearType;
+        }
+
+        /// <summary>
+        /// This will clear the first gear stats
+        /// </summary>
+        private void ClearFirstGearStats()
+        {
 
         }
 
-        private void PopulateGearStats()
+        /// <summary>
+        /// This will clear the second gear stats
+        /// </summary>
+        private void ClearSecondGearStats()
         {
 
         }
@@ -89,7 +162,9 @@ namespace MMO_Gear_Comparison_App
         private void btnClearSelectedGear_Click(object sender, EventArgs e)
         {
             cboFirstGear.SelectedIndex = -1;
+            ClearFirstGearStats();
             cboSecondGear.SelectedIndex = -1;
+            ClearSecondGearStats();
         }
     }
 }
