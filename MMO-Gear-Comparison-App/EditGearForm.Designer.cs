39eacfbcc6e153a5121bcbb1c7dd4f8d7c930407
Bart Ault <115667577+Bault496@users.noreply.github.com>
2023-08-31 01:40:30 -0700

    Created Selector for Edit Form

--- MMO-Gear-Comparison-App/EditGearForm.Designer.cs
+++ MMO-Gear-Comparison-App/EditGearForm.Designer.cs
@@ -28,45 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnCancel = new Button();
-            btnEditGear = new Button();
-            SuspendLayout();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnEditGear = new System.Windows.Forms.Button();
+            this.cboGearSelector = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
             // 
             // btnCancel
             // 
-            btnCancel.Location = new Point(658, 366);
-            btnCancel.Name = "btnCancel";
-            btnCancel.Size = new Size(130, 72);
-            btnCancel.TabIndex = 0;
-            btnCancel.Text = "Cancel Edit Gear";
-            btnCancel.UseVisualStyleBackColor = true;
-            btnCancel.Click += btnCancel_Click;
+            this.btnCancel.Location = new System.Drawing.Point(658, 366);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(130, 72);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "Cancel Edit Gear";
+            this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // btnEditGear
             // 
-            btnEditGear.Location = new Point(140, 344);
-            btnEditGear.Name = "btnEditGear";
-            btnEditGear.Size = new Size(158, 74);
-            btnEditGear.TabIndex = 1;
-            btnEditGear.Text = "Edit Selected Gear";
-            btnEditGear.UseVisualStyleBackColor = true;
-            btnEditGear.Click += btnEditGear_Click;
+            this.btnEditGear.Location = new System.Drawing.Point(140, 344);
+            this.btnEditGear.Name = "btnEditGear";
+            this.btnEditGear.Size = new System.Drawing.Size(158, 74);
+            this.btnEditGear.TabIndex = 1;
+            this.btnEditGear.Text = "Edit Selected Gear";
+            this.btnEditGear.UseVisualStyleBackColor = true;
+            // 
+            // cboGearSelector
+            // 
+            this.cboGearSelector.FormattingEnabled = true;
+            this.cboGearSelector.Location = new System.Drawing.Point(252, 30);
+            this.cboGearSelector.Name = "cboGearSelector";
+            this.cboGearSelector.Size = new System.Drawing.Size(159, 23);
+            this.cboGearSelector.TabIndex = 2;
             // 
             // EditGearForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(btnEditGear);
-            Controls.Add(btnCancel);
-            Name = "EditGearForm";
-            Text = "Edit Gear";
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cboGearSelector);
+            this.Controls.Add(this.btnEditGear);
+            this.Controls.Add(this.btnCancel);
+            this.Name = "EditGearForm";
+            this.Text = "Edit Gear";
+            this.Load += new System.EventHandler(this.EditGearForm_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
 
         private Button btnCancel;
         private Button btnEditGear;
+        private ComboBox cboGearSelector;
     }
 }
